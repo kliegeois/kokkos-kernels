c0b2f99072a30a55d2e742e07a394a00f0e0d280
kliegeois <kimliegeois@ymail.com>
2022-05-06 13:38:01 -0600

    Update RCI

--- perf_test/batched/sparse/python/run_GPTune.sh
+++ perf_test/batched/sparse/python/run_GPTune.sh
@@ -8,13 +8,13 @@ export MKL_DYNAMIC=TRUE
 export OMP_DYNAMIC=FALSE
 export OMP_NUM_THREADS=26
 export OMP_PROC_BIND=true
-export OMP_DISPLAY_AFFINITY=TRUE
+#export OMP_DISPLAY_AFFINITY=TRUE
 export OMP_PLACES=threads
-export OMP_DISPLAY_ENV=true
+#export OMP_DISPLAY_ENV=true
 
 rm -rf gptune.db/*.json # do not load any database 
 tp=batchedGMRES
-app_json=$(echo "{\"tuning_problem_name\":\"$tp\"")
+app_json=$(echo "{\"tuning_problem_name\":\"$tp\",\"no_load_check\": \"yes\"")
 echo "$app_json$machine_json$software_json$loadable_machine_json$loadable_software_json}" | jq '.' > .gptune/meta.json
-bash batchedGMRES_RCI.sh -a 1 -b 1 -c time
+bash batchedGMRES_RCI.sh -a 100 -b 1 -c time
 cp gptune.db/batchedGMRES.json  gptune.db/batchedGMRES.json_$(timestamp)
